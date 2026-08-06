b7b7c742504b0c7c78ab892b8e0ef0d2912f65d8
Edward West <edwardwestdev@gmail.com>
2026-08-06 13:06:25 -0700

    made insert mode cursor black in light mode

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,21 @@
 -- Add any additional options here
 
 vim.o.shell = "fish"
+
+local function update_cursor()
+  if vim.o.background == "light" then
+    -- black block cursor in insert mode for light themes
+    vim.cmd.highlight({ "iCursor", "guibg=#000000", "guifg=#ffffff" })
+    vim.opt.guicursor = "n-v-c-sm:block-Cursor,i-ci-ve:ver25-iCursor,r-cr-o:hor20-Cursor"
+  else
+    -- default cursor for dark themes
+    vim.cmd.highlight({ "iCursor", "guibg=NONE", "guifg=NONE" })
+    vim.opt.guicursor = "n-v-c-sm:block-Cursor,i-ci-ve:ver25-Cursor,r-cr-o:hor20-Cursor"
+  end
+end
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+  callback = update_cursor,
+})
+
+update_cursor()
