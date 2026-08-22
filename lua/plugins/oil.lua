2269f2f8ddf81b55eb78ddfd99accfa237ff3a56
Edward West <edwardwestdev@gmail.com>
2026-08-21 23:26:13 -0700

    installed oil

---
+++ lua/plugins/oil.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+    keys = {
+      { "-", "<cmd>Oil<cr>", desc = "Open parent directory (Oil)" },
+    },
+  },
+}
